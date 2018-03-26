fce7875fdc2fae36e914ed9068a495582ddeb2c2
tkpromise <tkpromise@foxmail.com>
2018-03-26 15:47:57 +0800

    datastore

---
+++ main.js
@@ -0,0 +1,12 @@
+function initializeEvents() {
+	'use strict';
+	var thumbnails = getThumbnailsArray();
+	thumbnails.forEach(addThumbClickHandler);
+}
+initializeEvents();
+
+
+(initializeEvents(thumbnails) {
+	thumbnails.forEach(addThumbClickHandler);
+})(getThu:q
+)
